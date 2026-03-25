f9cbec92210644039fb54e1eac7cbd258589ce73
Luis <luisrobalcantara@gmail.com>
2026-03-25 12:53:46 +0100

    Read through documentation to get some hardware names and to test something later at 3M

--- src/main.py
+++ src/main.py
@@ -1,17 +1,32 @@
 from time import sleep
+import sys
+import os
+from mirte_robot import robot
+
+# Test the wheel moving
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'pioneer-robot'))
+import hardware as hd
+
+# from "src/pioneer-robot" import hardware
 
 # Runs once at the start
 def setup():
     pass
 
-
 # Main entry point to the program
 def main():
     setup()
 
+    # Initializes hardware n shit
+    mirte = robot.createRobot()
+    hd.set_motor_speed(mirte, 0, 0)
+    
     while(1):
-        print("Doing nothing at all")
+        print("Moving the motors")
+        hd.set_motor_speed(mirte, 60, 60)
         sleep(1)
-
+        print("Stopping the motors")
+        hd.set_motor_speed(mirte, 0, 0)
+        sleep(2)
 
 main()
--- src/pioneer-robot/hardware.py
+++ src/pioneer-robot/hardware.py
@@ -4,10 +4,27 @@
 from cv2 import Mat
 
 # Example function
-def set_motor_speed(left_motor_speed: int, right_motor_speed: int) -> None:
+def set_motor_speed(mirte, left_motor_speed: int, right_motor_speed: int) -> None:
     NotImplemented
 
+    # mirte is an argument because we can use it to call ROS services
+    mirte.setMotorSpeed("left", left_motor_speed)
+    mirte.setMotorSpeed("right", right_motor_speed)
+
+    # ROS names for motors
+    # motor/left 
+    # motor/right
+
+def getDistances(mirte):
+    return [mirte.getDistance("left"), mirte.getDistance("right")]
+    
+    # ROS name for sonar sensors
+    # distance/left
+    # distance/right
 
 # Take a picture with the robot's camera and return it
-def capture_RGB_image() -> Mat:
+def capture_RGB_image(mirte) -> Mat:
     NotImplemented
+
+    # ROS topic for camera frame:
+    # /video1/image_raw/compressed
